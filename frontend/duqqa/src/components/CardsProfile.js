ae4b1bf49e164f89a89a8eae4b367be7000c2817
ggakila <gisorehaggai@gmail.com>
2024-03-29 17:39:32 +0300

    styling edits

--- frontend/duqqa/src/components/CardsProfile.js
+++ frontend/duqqa/src/components/CardsProfile.js
@@ -50,14 +50,16 @@ const handleDelete = async () => {
 				</div>
 			</Link>
 			<div className="flex py-5 justify-between items-center p-5">
-				<h3 className="text-sm font-semibold tracking-wide truncate">{productName}</h3>
-				<p className="font-medium">${price}</p>
+				<h3 className="text-sm font-semibold tracking-wide truncate">
+					{productName}
+				</h3>
+				<p className="font-medium text-green-700 mx-1">${price}</p>
 				<Link
 					href={{
 						pathname: "/edit/[slug]",
 						query: { slug: id },
 					}}
-					className=" p-2 text-xs text-center text-green-700  hover:bg-green-500 hover:text-gray-100"
+					className=" p-2 text-xs text-center   hover:bg-blue-500 hover:text-gray-100"
 				>
 					Edit
 				</Link>
